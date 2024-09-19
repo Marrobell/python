ff380c8a3f94b608210d4342e45fd7df5c7a833d
Marrobell <trol-nick@mail.ru>
2024-09-19 11:21:17 +0300

    Added new program. Program can make change in devices.txt and read it while working

---
+++ change-txt.py
@@ -0,0 +1,19 @@
+file = open("devices.txt", "r+")
+for item in file:
+    item = item.strip()
+    print(item)
+file.close()
+while True:
+    newtext = input("Enter 'T'ext to read file" "\n" "Enter new device name" "\n" "enter exit to close: ")
+    if newtext == "exit":
+        break
+    elif newtext == "T":
+        file = open("devices.txt", "r")
+        for item in file:
+            item = item.strip()
+            print(item)
+        file.close()
+    file = open("devices.txt", "r+")
+    file.write(newtext + "\n")
+    file.close()
+file.close()
